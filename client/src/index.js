21e075e751dfa0ca2664254646bee30afb6110ea
Wenqian Weng <136841453+wwqqq16@users.noreply.github.com>
2023-09-30 14:37:46 -0700

    Add files via upload

---
+++ client/src/index.js
@@ -0,0 +1,20 @@
+import 'materialize-css/dist/css/materialize.min.css';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+
+import App from './components/App';
+import reducers from './reducers';
+import axios from 'axios';
+window.axios = axios;
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
